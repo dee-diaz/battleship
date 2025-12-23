8e5a2b9a3d958f4f35ca451282df3320f25d597a
dee-diaz <diazthediablo@yahoo.com>
2025-12-23 13:47:46 -0300

    feat: create utility class Render and implement first static method for rendering the initial game screen - user name prompt

--- src/components/Render.js
+++ src/components/Render.js
@@ -1,5 +1,39 @@
 class Render {
-  renderFirstScreen() {}
+  static firstScreen() {
+    const userPrompt = document.createElement('div');
+    userPrompt.className = 'user-prompt';
+
+    const para = document.createElement('p');
+    para.textContent = 'What’s your name, cap?';
+
+    const form = document.createElement('form');
+    form.id = 'user-name-form';
+
+    const formControl = document.createElement('div');
+    formControl.className = 'form-control';
+
+    const input = document.createElement('input');
+    input.type = 'text';
+    input.name = 'user-name';
+    input.id = 'input-name';
+    input.placeholder = 'Enter your name';
+    input.minLength = 2;
+    input.required = true;
+
+    const btn = document.createElement('button');
+    btn.type = 'submit';
+    btn.textContent = 'Start game';
+
+    formControl.appendChild(input);
+
+    form.appendChild(formControl);
+    form.appendChild(btn);
+
+    userPrompt.appendChild(para);
+    userPrompt.appendChild(form);
+
+    return userPrompt;
+  }
 }
 
 export default Render;
